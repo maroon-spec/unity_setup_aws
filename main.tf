4bfb16dded067b660c657d770f9b206d844a0d47
maroon-spec <gont.jun@gmail.com>
2022-10-09 22:55:58 +0900

    update wait time

--- main.tf
+++ main.tf
@@ -14,6 +14,25 @@ module "aws_metastore" {
   unity_metastore_bucket = var.unity_metastore_bucket
 }
 
+
+/***************************************************************************************
+* wait 30 second for create aws resources
+****************************************************************************************/
+
+# This resource will destroy (potentially immediately) after null_resource.next
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "30s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_seconds]
+}
+
 /***************************************************************************************
 * Create the first Unity Catalog metastore and assign it to the chosen workspaces
 ****************************************************************************************/
